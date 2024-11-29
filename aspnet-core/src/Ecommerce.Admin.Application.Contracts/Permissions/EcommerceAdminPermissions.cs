b96f1484ac3a2fdf9abf1c498d2b1ad4ee7fe562
huytq@ics-p.vn <huytq@ics-p.vn>
2024-11-29 09:05:41 +0700

    Init structure project

---
+++ aspnet-core/src/Ecommerce.Admin.Application.Contracts/Permissions/EcommerceAdminPermissions.cs
@@ -0,0 +1,9 @@
+﻿namespace Ecommerce.Admin.Permissions;
+
+public static class EcommerceAdminPermissions
+{
+    public const string GroupName = "Ecommerce";
+
+    //Add your own permission names. Example:
+    //public const string MyPermission1 = GroupName + ".MyPermission1";
+}
---
+++ aspnet-core/test/Ecommerce.Admin.Application.Tests/EcommerceAdminApplicationTestBase.cs
@@ -0,0 +1,6 @@
+﻿namespace Ecommerce.Admin;
+
+public abstract class EcommerceAdminApplicationTestBase : EcommerceTestBase<EcommerceAdminApplicationTestModule>
+{
+
+}
