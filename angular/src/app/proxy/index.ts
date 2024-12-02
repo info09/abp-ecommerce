409a3d57b1fb28cd4158996c82fdcaa1876eeee2
huytq@ics-p.vn <huytq@ics-p.vn>
2024-12-02 09:20:03 +0700

    feat: Generate proxy using ABP CLI

---
+++ angular/src/app/proxy/index.ts
@@ -0,0 +1,5 @@
+import * as Ecommerce from './ecommerce';
+import * as ProductCategories from './product-categories';
+import * as Products from './products';
+export * from './models';
+export { Ecommerce, ProductCategories, Products };
