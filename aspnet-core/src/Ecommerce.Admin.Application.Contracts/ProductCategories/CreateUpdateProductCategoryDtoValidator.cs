5986c342235421b13759d48cb41c126db596dd8c
huytq@ics-p.vn <huytq@ics-p.vn>
2024-12-02 10:51:49 +0700

    feat: Add fluent validator for DTO

---
+++ aspnet-core/src/Ecommerce.Admin.Application.Contracts/ProductCategories/CreateUpdateProductCategoryDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Ecommerce.Admin.ProductCategories
+{
+    public class CreateUpdateProductCategoryDtoValidator : AbstractValidator<CreateUpdateProductCategoryDto>
+    {
+        public CreateUpdateProductCategoryDtoValidator()
+        {
+            RuleFor(i => i.Name).NotEmpty().MaximumLength(50);
+            RuleFor(i => i.Code).NotEmpty().MaximumLength(50);
+            RuleFor(i => i.Slug).NotEmpty().MaximumLength(50);
+            RuleFor(x => x.CoverPicture).MaximumLength(250);
+            RuleFor(x => x.SeoMetaDescription).MaximumLength(250);
+        }
+    }
+}
